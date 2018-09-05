efec2bbf67522ffaaa05b4c5035f2a7dafe91561
SushantAgrawal <sagarwal@netwoven.com>
2018-09-05 08:44:09 +0530

    init

--- api/tear/sql.js
+++ api/tear/sql.js
@@ -1,12 +1,17 @@
+//, NonBillable=SUM(ActualWorkNonBillable)
 let sql = {
     hours:`
     select r.FullName, ProjectName, Date=CAST(Date AS DATE),   
-Billable=SUM(ActualWorkBillable), NonBillable=SUM(ActualWorkNonBillable),  WorkDays=1456
+Billable=SUM(ActualWorkBillable), c.ClassName
 	from NWTA_TimeSheet t
 		join NWTA_Resource r
 			on r.ResourceUID = t.OwnerResourceUID
 		join NWTA_TimesheetLine l
 			on l.TimesheetUID = t.TimesheetUID
+
+		join NWTA_TimesheetClass c
+			on l.ClassID = c.TimesheetClassID
+
 		join NWTA_TimesheetActual a
 			on a.TimesheetLineUID = l.TimesheetLineUID
 		join NWTA_Assignment n
@@ -18,8 +23,9 @@ Billable=SUM(ActualWorkBillable), NonBillable=SUM(ActualWorkNonBillable),  WorkD
 	where 	
 		a.Date >= '{{dateFrom}}'
 		and a.Date <= '{{dateTo}}'
-		and (ActualWorkBillable > 0 or ActualWorkNonBillable > 0)
-	GROUP by r.FullName, ProjectName, Date
+		and (ActualWorkBillable > 0)
+		-- and classID = 1
+	GROUP by r.FullName, ProjectName, Date, ClassName
 	order by r.FullName, Date
     `
     ,hours1: `select r.FullName, ProjectName, TimesheetName,   
