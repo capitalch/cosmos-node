c650ea3afe7682efe810fff6de4125994e8f7add
SushantAgrawal <sagarwal@netwoven.com>
2018-09-18 11:14:28 +0530

    init

--- tools/postgres/postgres.js
+++ tools/postgres/postgres.js
@@ -14,22 +14,43 @@ const postgres = {};
 //     max: 10, // max number of clients in the pool
 //     idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
 // };
+function getParameterizedSql(sql, paramObject) {
+    const matchet = {
+        counter: 1,
+        getParam: () => '$'.concat(matchet.counter++),
+        result: {
+            sql: sql,
+            values: []
+        }
+    };
+
+    paramObject && Object.keys(paramObject).forEach(x => {
+        let paramName = matchet.getParam();
+        matchet.result.values.push(paramName);
+        result.sql = result.sql.split(':'.concat(x)).join(paramName);
+    });
+    return (matchet.result);
+}
+
 const pool = new Pool(config['system:postgres']);
+// postgres.exec = (context, sql) => {
 postgres.exec = (context, sql) => {
     // pool.connect()
     //     .then(client => {
-            pool.query('SELECT * FROM apidata')
-                .then(res => {
-                    // client.release();
-                    context.res.json(res.rows);
-                    logger.doLog('info',messages.messQueryExecuted,null);
-                })
-                .catch(e => {
-                    context.res.locals.message = messages.errQueryFalied(config['system:postgres'].database)
-                    context.next(e.message);
-                })
-        // }).catch(e => {
-        //     console.log(e);
-        // });
+    const sql = 'SELECT * FROM contacts';
+    pool.query(getParameterizedSql(sql,''))
+    // pool.query('SELECT * FROM contacts')
+        .then(res => {
+            // client.release();
+            context.res.json(res.rows);
+            logger.doLog('info', messages.messQueryExecuted, null);
+        })
+        .catch(e => {
+            context.res.locals.message = messages.errQueryFalied(config['system:postgres'].database)
+            context.next(e.message);
+        })
+    // }).catch(e => {
+    //     console.log(e);
+    // });
 }
 module.exports = postgres;
