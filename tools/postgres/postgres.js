c7bd77b10539c8220ad9b14c201ecc156bcabb5f
SushantAgrawal <sagarwal@netwoven.com>
2018-09-18 14:41:28 +0530

    init

--- tools/postgres/postgres.js
+++ tools/postgres/postgres.js
@@ -1,19 +1,13 @@
+"use strict";
 const pg = require('pg');
 const format = require('pg-format');
 const messages = require('../../common/messages');
 const logger = require('../../common/logger')('system');
 const { Pool } = require('pg');
-const sql = require('./sql');
+// const sql = require('./sql');
 const config = require('../../common/config.json');
-const postgres = {};
-// const dbConfig = {
-//     user: config.user, // name of the user account
-//     database: config.database, // name of the database
-//     password: config.password,
-//     port: config.dbPort,
-//     max: 10, // max number of clients in the pool
-//     idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
-// };
+let postgres = {};
+
 function getParameterizedSql(sql, paramObject) {
     const matchet = {
         counter: 1,
@@ -33,15 +27,11 @@ function getParameterizedSql(sql, paramObject) {
 }
 
 const pool = new Pool(config['system:postgres']);
-// postgres.exec = (context, sql) => {
+
 postgres.exec = (context, sql) => {
-    // pool.connect()
-    //     .then(client => {
-    const sql = 'SELECT * FROM contacts';
-    pool.query(getParameterizedSql(sql,''))
-    // pool.query('SELECT * FROM contacts')
+    sql && (sql = 'SELECT * FROM contacts');
+    pool.query(getParameterizedSql(sql))
         .then(res => {
-            // client.release();
             context.res.json(res.rows);
             logger.doLog('info', messages.messQueryExecuted, null);
         })
@@ -49,8 +39,14 @@ postgres.exec = (context, sql) => {
             context.res.locals.message = messages.errQueryFalied(config['system:postgres'].database)
             context.next(e.message);
         })
-    // }).catch(e => {
-    //     console.log(e);
-    // });
 }
 module.exports = postgres;
+//
+// const dbConfig = {
+//     user: config.user, // name of the user account
+//     database: config.database, // name of the database
+//     password: config.password,
+//     port: config.dbPort,
+//     max: 10, // max number of clients in the pool
+//     idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+// };
