9f3b50db5596a7cd6f1cb3dc8b67d5e9a06f0d22
SushantAgrawal <sagarwal@netwoven.com>
2019-04-24 21:23:21 +0530

    init

--- apps/eshop/artifacts/postgres.js
+++ apps/eshop/artifacts/postgres.js
@@ -18,8 +18,8 @@ const dbConfig = {
   password: config.db.password,
   port: config.db.port,
   host: config.db.host,
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+  max: 3, // max number of clients in the pool
+  idleTimeoutMillis: 3000 // how long a client is allowed to remain idle before being closed
 };
 
 const pool = new Pool(dbConfig);
