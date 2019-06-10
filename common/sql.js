05fcef5bc293ec6d85586731ae3c63a2a49e172e
capitalch <capitalch@gmail.com>
2019-06-10 14:05:17 +0530

    init

--- common/sql.js
+++ common/sql.js
@@ -47,7 +47,7 @@ const sql = {
     `
 
     , 'id:get-comments':`
-        select id, parent_id,mname, comment from comments 
+        select id, parent_id,mname, comment, commented_on from comments 
     `
 
 }
